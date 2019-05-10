4b3b39eab430a0d4adb49881436cac726bc15629
jjuan01 <jpleyvat@gmail.com>
2019-05-09 19:58:13 -0500

    Clase 16: Miltiples componentes en una pagina

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,32 @@ import BadgeForm from '../components/BadgeForm';
 import Badge from '../components/Badge';
 
 class BadgeNew extends React.Component {
+	state = {
+		form: {
+			firstName: '',
+			lastName: '',
+			email: '',
+			jobTitle: '',
+			twitter: ''
+		}
+	};
+
+	handleChange = e => {
+		// const nextForm = this.state.form;
+		// nextForm[e.target.name] = e.target.value;
+
+		// this.setState({
+		//   form: nextForm
+		// });
+
+		this.setState({
+			form: {
+				...this.state.form,
+				[e.target.name]: e.target.value
+			}
+		});
+	};
+
 	render() {
 		return (
 			<div>
@@ -18,19 +44,31 @@ class BadgeNew extends React.Component {
 						alt='Logo'
 					/>
 				</div>
+
 				<div className='container'>
 					<div className='row'>
 						<div className='col-6'>
 							<Badge
-								firstName='Richard'
-								lastName='Kaufman'
-								twitter='sparragus'
-								jobTitle='Frontend Engineer'
+								firstName={
+									this.state.form.firstName
+								}
+								lastName={
+									this.state.form.lastName
+								}
+								twitter={this.state.form.twitter}
+								jobTitle={
+									this.state.form.jobTitle
+								}
+								email={this.state.form.email}
 								avatarUrl='https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon'
 							/>
 						</div>
+
 						<div className='col-6'>
-							<BadgeForm />
+							<BadgeForm
+								onChange={this.handleChange}
+								formValues={this.state.form}
+							/>
 						</div>
 					</div>
 				</div>
