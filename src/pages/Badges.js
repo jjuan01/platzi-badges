90155d9e477edfe0f5250f6d80c886d2de6c4e35
jjuan01 <jpleyvat@gmail.com>
2019-05-11 11:19:42 -0500

    Clase 18: React router <BrowserRouter>, <Switch>, <Route>

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/Badges.css';
 
@@ -62,12 +63,12 @@ class Badges extends React.Component {
 				</div>
 				<div className='Badges__container'>
 					<div className='Badges__buttons'>
-						<a
-							href='/badges-new'
+						<Link
+							to='/badges/new'
 							className='btn btn-primary'
 						>
 							New badge
-						</a>
+						</Link>
 					</div>
 
 					<div className='Badges__list'>
