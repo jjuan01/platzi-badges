8e6f3554c92c9f1f44febec96305f499b3ee86b1
jjuan01 <jpleyvat@gmail.com>
2019-05-18 14:46:17 -0500

    Fixes bugs after merging

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import de elemento con javascript
 // const element = document.createElement("h1");
 // element.innerText = "Hello, Platzi Badges!";
@@ -8,17 +7,11 @@
 // container.appendChild(element);
 
 // import de elemento con react
-=======
->>>>>>> 83d0fcf79ddce76784ccf736000b355ad82660a3
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-<<<<<<< HEAD
-
-=======
->>>>>>> 83d0fcf79ddce76784ccf736000b355ad82660a3
 import App from './components/App';
 
 const container = document.getElementById('app');
